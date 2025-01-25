1874806c6abe61edcde545e64321e706943c5264
Greg Finley <gregory.finley@gmail.com>
2025-01-24 22:11:44 -0800

    MateCheckCapture

---
+++ mate_check_capture.py
@@ -0,0 +1,46 @@
+import chess
+from chess.engine import PlayResult
+from typing import Any
+import random
+
+
+class ExampleEngine:
+    pass
+
+
+class MateCheckCapture(ExampleEngine):
+    def search(self, board: chess.Board, *args: Any) -> PlayResult:
+        """Simple priorities: Checkmate > Check > Capture > Advance > Random."""
+        checkmating_moves = []
+        checking_moves = []
+        capturing_moves = []
+        advancing_moves = []
+        random_moves = []
+        for move in board.legal_moves:
+            if board.is_capture(move):
+                capturing_moves.append(move)
+            board.push(move)
+            if board.is_checkmate():
+                checkmating_moves.append(move)
+            elif board.is_check():
+                checking_moves.append(move)
+            else:
+                move_uci = move.uci()
+                is_going_upboard = int(move_uci[1]) > int(move_uci[3])
+                is_going_downboard = int(move_uci[1]) < int(move_uci[3])
+                if board.turn == chess.WHITE and is_going_upboard:
+                    advancing_moves.append(move)
+                elif board.turn == chess.BLACK and is_going_downboard:
+                    advancing_moves.append(move)
+                else:
+                    random_moves.append(move)
+            board.pop()
+        if checkmating_moves:
+            return PlayResult(random.choice(checkmating_moves), None)
+        elif checking_moves:
+            return PlayResult(random.choice(checking_moves), None)
+        elif capturing_moves:
+            return PlayResult(random.choice(capturing_moves), None)
+        elif advancing_moves:
+            return PlayResult(random.choice(advancing_moves), None)
+        return PlayResult(random.choice(random_moves), None)
---
+++ test_mate_check_capture.py
@@ -0,0 +1,62 @@
+import chess
+import pytest
+from mate_check_capture import MateCheckCapture
+
+
+@pytest.fixture(autouse=True)
+def engine():
+    return MateCheckCapture()
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_prefers_mate_over_check(execution_number, engine):
+    """Should choose checkmate even when check is available"""
+    board = chess.Board("4k3/2R5/4K3/8/8/8/8/8 w - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "c7c8"
+    board.push(move)
+    assert board.is_checkmate()
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_prefers_check_over_capture(execution_number, engine):
+    """Should choose check even when capture is available"""
+    board = chess.Board("4k3/4r3/8/8/5r2/4P3/r7/Qn2K3 w - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "a1h8"
+    board.push(move)
+    assert board.is_check()
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_prefers_capture_over_advance(execution_number, engine):
+    """Should choose capture even when advance is available"""
+    board = chess.Board("1b2k3/1P2r3/5b1N/8/5r2/1N2N3/P7/QN2K3 w - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "a1f6"
+    board.push(move)
+    assert board.is_capture(move)
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_advances_white(execution_number, engine):
+    """Should advance up the board as white"""
+    board = chess.Board("8/3k4/8/6r1/6P1/6PP/6PP/7K w - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "h3h4"
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_advances_black(execution_number, engine):
+    """Should advance down the board as black"""
+    board = chess.Board("8/k7/pp6/1p6/1N4P1/6PP/6PP/7K b - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "a6a5"
+
+
+@pytest.mark.parametrize("execution_number", range(100))
+def test_play_random(execution_number, engine):
+    """Should do something if no advances or other options"""
+    board = chess.Board("KR6/BR6/8/8/8/8/1ppppppp/bkbbbbbb b - - 0 1")
+    move = engine.search(board).move
+    assert move.uci() == "b1a2"
